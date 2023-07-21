b8b64287cf08920fc39b8e0eeee3b4a685042a5c
Aarni Halinen <aarni.halinen@gmail.com>
2023-07-21 23:12:47 +0300

    Update iptables scripts

--- thesis/code/iptables.sh
+++ thesis/code/iptables.sh
@@ -6,18 +6,18 @@ SIDECAR_PORT=8125
 
 # Ingress rules
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -p tcp --dport $APP_PORT -j ACCEPT # Scenarios 1, 2
-iptables -A INPUT -i lo -p tcp --dport $SIDECAR_PORT -j ACCEPT # Scenario 3
+iptables -A INPUT -p tcp --dport $APP_PORT -j ACCEPT
+iptables -A INPUT -p udp --dport $SIDECAR_PORT -j ACCEPT
 iptables -A INPUT -j DROP
 
 # Egress rules
 iptables -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 ## Main app rules
-iptables -A OUTPUT -d 127.0.0.1 -m owner --uid-owner $APP_UID -p tcp --dport $APP_PORT -j ACCEPT # Scenario 2
-iptables -A OUTPUT -d 127.0.0.1 -m owner --uid-owner $APP_UID -p tcp --dport $SIDECAR_PORT -j ACCEPT # Scenario 2
+iptables -A OUTPUT -o lo -m owner --uid-owner $APP_UID -p tcp --dport $APP_PORT -j ACCEPT
+iptables -A OUTPUT -o lo -m owner --uid-owner $APP_UID -p udp --dport $SIDECAR_PORT -j ACCEPT
 
 ## Sidecar rules
-iptables -A OUTPUT -d 127.0.0.1 -m owner --uid-owner $SIDECAR_UID -p tcp --dport $SIDECAR_PORT -j ACCEPT
+iptables -A OUTPUT -o lo -m owner --uid-owner $SIDECAR_UID -p udp --dport $SIDECAR_PORT -j ACCEPT
 
 iptables -A OUTPUT -j REJECT
--- thesis/code/localhost.sh
+++ thesis/code/localhost.sh
@@ -1,4 +1,4 @@
-    #!/bin/bash
-    sysctl -w net.ipv4.conf.all.route_localnet=1
-    iptables -t nat -A OUTPUT -p ALL --dport 8125 -j DNAT --to-destination 192.168.1.202:8125
-    iptables -t nat -A POSTROUTING -j MASQUERADE
+#!/bin/bash
+sysctl -w net.ipv4.conf.all.route_localnet=1
+iptables -t nat -A OUTPUT -m addrtype --src-type LOCAL --dst-type LOCAL -p udp --dport 8125 -j DNAT --to-destination 192.168.1.202:8125
+iptables -t nat -A POSTROUTING -m addrtype --src-type LOCAL --dst-type UNICAST -j MASQUERADE
